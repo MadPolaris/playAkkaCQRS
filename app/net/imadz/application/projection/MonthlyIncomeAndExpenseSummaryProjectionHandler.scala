e0f1faedbb40f3284a9a8c10e02b160e336f8c66
barry <zhongdj@gmail.com>
2024-06-30 08:51:27 +0800

    adapt to protobuf event source

--- app/net/imadz/application/projection/MonthlyIncomeAndExpenseSummaryProjectionHandler.scala
+++ app/net/imadz/application/projection/MonthlyIncomeAndExpenseSummaryProjectionHandler.scala
@@ -5,26 +5,32 @@ import akka.projection.eventsourced
 import akka.projection.eventsourced.EventEnvelope
 import akka.projection.jdbc.scaladsl.JdbcHandler
 import net.imadz.application.projection.repository.MonthlyIncomeAndExpenseSummaryRepository
-import net.imadz.domain.entities.CreditBalanceEntity.{BalanceChanged, CreditBalanceEvent}
+import net.imadz.domain.entities.CreditBalanceEntity.BalanceChanged
+import net.imadz.infrastructure.persistence.CreditBalanceEventAdapter
+import net.imadz.infrastructure.proto.credits.{CreditBalanceEventPO => CreditEventPO}
 
 import java.time.{Instant, LocalDateTime, ZoneId}
 
 case class MonthlyIncomeAndExpenseSummaryProjectionHandler(sharding: ClusterSharding,
-                                                           repository: MonthlyIncomeAndExpenseSummaryRepository) extends JdbcHandler[eventsourced.EventEnvelope[CreditBalanceEvent], ScalikeJdbcSession] {
+                                                           repository: MonthlyIncomeAndExpenseSummaryRepository) extends JdbcHandler[eventsourced.EventEnvelope[CreditEventPO.Event], ScalikeJdbcSession] {
+  val adapter = new CreditBalanceEventAdapter
 
-  override def process(session: ScalikeJdbcSession, envelope: EventEnvelope[CreditBalanceEvent]): Unit = {
-    envelope.event match {
+  override def process(session: ScalikeJdbcSession, envelope: EventEnvelope[CreditEventPO.Event]): Unit = {
+    adapter.fromJournal(envelope.event, "").events.foreach {
       case BalanceChanged(update, timestamp) =>
         val (year, month, day) = getDateFromTimestamp(timestamp)
         val userId = envelope.persistenceId
         if (update.amount > 0)
           repository.updateIncome(userId, update.amount, year, month, day)
-        else
-          repository.updateExpense(userId, update.amount, year, month, day)
+        else {
+          repository.updateExpense(userId, -update.amount, year, month, day)
+        }
+      case _ =>
+        ()
     }
   }
 
-  def getDateFromTimestamp(timestamp: Long): (Int, Int, Int) = {
+  private def getDateFromTimestamp(timestamp: Long): (Int, Int, Int) = {
     val instant = Instant.ofEpochMilli(timestamp)
     val dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
     val year = dateTime.getYear
